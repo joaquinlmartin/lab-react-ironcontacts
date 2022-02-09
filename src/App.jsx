58ebfb2ee1f88151c85315bfcac87cd4b226683f
joaquinlmartin <joaquinlmartin@gmail.com>
2022-02-09 10:37:05 +0100

    its 1-5 done

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,28 @@ function App() {
     setContacts(contacts.concat(randomActors))
   }
 
+  const sortByPopularity = () => {
+    const sortPopularity = [...contacts].sort((actorA, actorB) => actorB.popularity - actorA.popularity);
+    setContacts(sortPopularity);
+  }
+
+  const sortByName = () => {
+    const sortName = [...contacts].sort((actorA, actorB) => actorA.name.localeCompare(actorB.name));
+    setContacts(sortName);
+  }
+
+  const deleteContacts = (contactId) => {
+    const filteredContacts = contacts.filter((contact) => {
+      return contact.id !== contactId
+    })
+    setContacts(filteredContacts);
+  }
+
   return <div className="App">
     <div className="header"><h1>IronContacts</h1></div>
     <button onClick={addRandomContacts}>Add random contacts</button>
+    <button onClick={sortByPopularity}>Sort by popularity</button>
+    <button onClick={sortByName}>Sort by name</button>
       <table className='Table'>
         <thead>
           <th>Picture</th>
@@ -34,6 +53,7 @@ function App() {
                 <td>{actors.popularity.toFixed(2)}</td>
                 <td>{actors.wonOscar ? <p>🏆</p> : <p>💩</p>}</td>
                 <td>{actors.wonEmmy ? <p>✨</p> : <p>💩</p>}</td>
+                <td><button onClick={() => deleteContacts(actors.id)}>Delete</button></td>
               </tr>
             )
           })}
